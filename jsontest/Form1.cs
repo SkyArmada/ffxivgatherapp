49d235a9d378f44ff81a6184b7265748ca67ceed
SkyArmada <sky.armada+Gamedev@gmail.com>
2015-03-06 16:30:05 -0600

    initial commit

---
+++ jsontest/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+namespace jsontest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filepath = "json\\item_nodes.json";
+
+            LoadJson();
+        }
+
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader(@"C:\Users\rellzy\Documents\Visual Studio 2013\Projects\jsontest\jsontest\json\item_nodes.json"))
+            {
+                string json = r.ReadToEnd();
+                List<item_node> items = JsonConvert.DeserializeObject<List<item_node>>(json);
+            }
+        }
+    }
+}
---
+++ jsontest/item_node.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsontest
+{
+    class item_node
+    {
+        private string type, name, location, time;
+        private int slot, star;
+
+        public item_node()
+        {
+
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+            set
+            {
+                location = value;
+            }
+        }
+
+        public int Slot
+        {
+            get
+            {
+                return slot;
+            }
+            set
+            {
+                slot = value;
+            }
+        }
+
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+            }
+        }
+
+        public int Star
+        {
+            get
+            {
+                return star;
+            }
+            set
+            {
+                star = value;
+            }
+        }
+    }
+}
